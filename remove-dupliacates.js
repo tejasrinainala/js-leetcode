1a6d052f096377015c9ee32907e168d2ce435d9d
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-11-09 20:11:58 +0530

    Create remove-dupliacates.js

---
+++ remove-dupliacates.js
@@ -0,0 +1,10 @@
+var removeDuplicates = function(nums) {
+    let i = nums.length - 1;
+    while (i > 0) {
+        if (nums[i] == nums[i - 1]) {
+            nums.splice(i, 1);
+        }
+        i--;
+    }
+    return nums.length;
+};
