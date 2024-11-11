918857b92b9ef1071b464586886cc43ab2963e11
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-11-11 19:40:35 +0530

    Create merge-strings-alternatively.js

---
+++ merge-strings-alternatively.js
@@ -0,0 +1,26 @@
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+  
+
+
+var mergeAlternately = function(word1, word2) {
+    let merged = ""
+    let maxLength=Math.max(word1.length,word2.length)
+    for(let i=0;i<maxLength;i++)
+    {
+        if(i<word1.length)
+        {
+            merged+=word1[i]
+        }
+       if(i<word2.length)
+       {
+        merged+=word2[i]
+       }
+       
+    }
+    return merged;
+};
