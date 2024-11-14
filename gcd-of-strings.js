9c0d55c57484b439fb2790087a10ba803ac46387
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-11-14 17:51:05 +0530

    Update gcd-of-strings.js

--- gcd-of-strings.js
+++ gcd-of-strings.js
@@ -1,3 +1,16 @@
+// For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+// Example 1:
+
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+    
+
+
 var gcdOfStrings = function (str1, str2) {
     if (str1 + str2 !== str2 + str1) {
         return "";
