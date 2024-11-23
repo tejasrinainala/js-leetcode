af47d0fbad7cf39e731a68a75fbb10dcf6834c32
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-11-23 22:07:05 +0530

    Update merge-strings-alternatively.js

--- merge-strings-alternatively.js
+++ merge-strings-alternatively.js
@@ -7,7 +7,7 @@
   
 
 
-var mergeAlternately = function(word1, word2) {
+let mergeAlternately = function(word1, word2) {
     let merged = ""
     let maxLength=Math.max(word1.length,word2.length)
     for(let i=0;i<maxLength;i++)
