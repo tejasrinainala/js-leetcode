56e481ae7f82ccf6537b7702bc362ebfadfe7fe0
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-12-09 19:23:43 +0530

    Update array-reduce.js

--- array-reduce.js
+++ array-reduce.js
@@ -1,4 +1,16 @@
-let reduce = function (nums, fn, init) {
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr; }
+// init = 0
+// Output: 10
+// Explanation:
+// initially, the value is init=0.
+// (0) + nums[0] = 1
+// (1) + nums[1] = 3
+// (3) + nums[2] = 6
+// (6) + nums[3] = 10
+// The final answer is 10.
+    let reduce = function (nums, fn, init) {
     let result = init;
     for (let i = 0; i < nums.length; i++) {
         result = fn(result, nums[i]);
