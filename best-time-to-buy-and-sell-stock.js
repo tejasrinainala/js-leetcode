1c23310d9a2d04db7e1a678946cb3fb26758804e
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-12-12 19:14:36 +0530

    Update best-time-to-buy-and-sell-stock.js

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -1,4 +1,10 @@
-var maxProfit = function (prices) {
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ var maxProfit = function (prices) {
     if (prices.length === 0) return 0;
 
     let minPrice = prices[0];
