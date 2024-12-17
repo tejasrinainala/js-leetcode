deab55bc46d9261cbbd54cf8d17fe91ed9ec70c7
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-12-17 20:03:24 +0530

    Update can-place-flowers.js

--- can-place-flowers.js
+++ can-place-flowers.js
@@ -1,4 +1,10 @@
-let canPlaceFlowers = function (flowerbed, n) {
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+    
+    let canPlaceFlowers = function (flowerbed, n) {
     flowerbed.unshift(0);
     flowerbed.push(0);
     for (let i = 1; i < flowerbed.length - 1; i++) {
