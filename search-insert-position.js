8b3747d07d4a18d9dc87aa3a55883b31363dcf02
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-12-18 18:59:19 +0530

    Update search-insert-position.js

--- search-insert-position.js
+++ search-insert-position.js
@@ -1,4 +1,7 @@
-var searchInsert = function (nums, target) {
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+
+    var searchInsert = function (nums, target) {
     if (nums.includes(target)) {
         return nums.indexOf(target)
     }
