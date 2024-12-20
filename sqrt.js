c7530a0ae75c06442d013c1a74e6596e6abbec67
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-12-20 20:30:02 +0530

    Update sqrt.js

--- sqrt.js
+++ sqrt.js
@@ -1,4 +1,10 @@
-let mySqrt = function(x) {
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+    let mySqrt = function(x) {
     let y=Math.sqrt(x)
     return Math.floor(y)
 };
