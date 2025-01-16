8bbd0a145c47ca73379d2f60da2e0ee7225cdcc2
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-01-16 19:19:05 +0530

    Update increasing-triplet-subsequence.js

--- increasing-triplet-subsequence.js
+++ increasing-triplet-subsequence.js
@@ -1,3 +1,11 @@
+// Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 
+
+
+
+
+
 var increasingTriplet = function(nums) {
     let c=0
     for(let i=0;i<nums.length;i++)
