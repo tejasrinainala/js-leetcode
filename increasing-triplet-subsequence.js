df70087ad5725d80602b43be7d1af1779db9e147
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-01-16 19:18:39 +0530

    Create increasing-triplet-subsequence.js

---
+++ increasing-triplet-subsequence.js
@@ -0,0 +1,17 @@
+var increasingTriplet = function(nums) {
+    let c=0
+    for(let i=0;i<nums.length;i++)
+    {
+        for(let j=i+1;j<nums.length;j++)
+        {
+            for(let k=j+1;k<nums.length;k++)
+            {
+                if(i<j<k && nums[i]<nums[j]<nums[k])
+                {
+                    return true
+                }
+            }
+        }
+    }
+    return false
+};
