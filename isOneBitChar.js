a4bee640d4aa22c8a87f87583ef0920c3d53060c
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-01-17 11:28:32 +0530

    Update isOneBitChar.js

--- isOneBitChar.js
+++ isOneBitChar.js
@@ -1,3 +1,13 @@
+// We have two special characters:
+
+// The first character can be represented by one bit 0.
+// The second character can be represented by two bits (10 or 11).
+// Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+
+
+
+
+
 var isOneBitCharacter = function(bits) {
     let i=0;
     while(i<bits.length-1)
