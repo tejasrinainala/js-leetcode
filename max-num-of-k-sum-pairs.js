4f21ab180a829cf25078836156ff66c3c4b15bf8
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-01-18 18:24:50 +0530

    Update max-num-of-k-sum-pairs.js

--- max-num-of-k-sum-pairs.js
+++ max-num-of-k-sum-pairs.js
@@ -1,3 +1,18 @@
+// You are given an integer array nums and an integer k.
+
+// In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+// Return the maximum number of operations you can perform on the array.
+
+ 
+
+
+
+
+
+
+
+
 var maxOperations = function(nums, k) {
     let count = 0;
     const map = new Map();
