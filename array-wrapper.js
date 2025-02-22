c2e9c18f59e07b29bfefd011bac5a74446802075
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-02-22 18:42:46 +0530

    Create array-wrapper.js

---
+++ array-wrapper.js
@@ -0,0 +1,18 @@
+var ArrayWrapper = function(nums) {
+    this.nums = nums;  
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((sum, num) => sum + num, 0); 
+};
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums.join(",")}]`; 
+}
+
